d80084a9274e06097cccc6f51b5c4a41dda03b02
Bakso14 <sayyidul98@gmail.com>
2020-09-14 10:16:06 +0700

    Pembersihan bagian tidak penting
    bagian-bagian seperti morph dan arah2

--- Main_load.py
+++ Main_load.py
@@ -3,25 +3,6 @@ import numpy as np
 from math import atan2, cos, sin, sqrt, pi
 def nothing(x):
     pass
-
-def drawAxis(img, p_, q_, colour, scale):
-    p = list(p_)
-    q = list(q_)
-    
-    angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
-    print(angle)
-    hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
-    # Here we lengthen the arrow by a factor of scale
-    q[0] = p[0] - scale * hypotenuse * cos(angle)
-    q[1] = p[1] - scale * hypotenuse * sin(angle)
-    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
-    # create the arrow hooks
-    p[0] = q[0] + 9 * cos(angle + pi / 4)
-    p[1] = q[1] + 9 * sin(angle + pi / 4)
-    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
-    p[0] = q[0] + 9 * cos(angle - pi / 4)
-    p[1] = q[1] + 9 * sin(angle - pi / 4)
-    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
     
 def getOrientation(pts, img):
     
@@ -38,10 +19,6 @@ def getOrientation(pts, img):
     
         
     cv2.circle(img, cntr, 3, (255, 0, 255), 2)
-    #p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
-    #p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
-    #drawAxis(img, cntr, p1, (0, 255, 0), 100)
-    #drawAxis(img, cntr, p2, (255, 255, 0), 100)
     angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
     length = 100
     x2 = cntr[0] + length*cos(angle)
@@ -72,12 +49,8 @@ while(1):
     crop = frame[200:360,0:640]
     
     hsv = cv2.cvtColor(crop,cv2.COLOR_BGR2HSV)
-
     warna = cv2.inRange(hsv, HSV_Low, HSV_High)
-    #res = cv2.bitwise_and(frame,frame, mask = mask)
     erosion = cv2.erode(warna,kernel,iterations = 1)
-    #dilation = cv2.dilate(mask,kernel,iterations = 1)
-    #mask = cv2.morphologyEx(warna, cv2.MORPH_OPEN, kernel)
     
     contours = cv2.findContours(warna, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
     for i, c in enumerate(contours):
@@ -96,12 +69,6 @@ while(1):
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(crop,[box],0,(255,0,0),2)
-        
-        #rows,cols = frame.shape[:2]
-        #[vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
-        #lefty = int((-x*vy/vx) + y)
-        #righty = int(((cols-x)*vy/vx)+y)
-        #cv2.line(crop,(cols-1,righty),(0,lefty),(0,255,0),2)
         
         a = getOrientation(c, crop)
         print('a =',a)
