18cbcc74e37a96e31decf32ae963d5e9386e801e
Bakso14 <sayyidul98@gmail.com>
2020-09-16 12:49:12 +0700

    Penambahan Contoh mudah
    gambar buatan sedniri
    penyimpan orientasi setiap connected component

--- Main_load.py
+++ Main_load.py
@@ -43,6 +43,7 @@ def getOrientation(pts, img):
     #drawAxis(img, cntr, p1, (0, 255, 0), 100)
     #drawAxis(img, cntr, p2, (255, 255, 0), 100)
     angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+    #angle = -15*(2*pi)/360
     length = 100
     x2 = cntr[0] + length*cos(angle)
     y2 = cntr[1] + length*sin(angle)
@@ -61,7 +62,9 @@ while(1):
     HSV_High = data[1,:]
         
     #_,src = cap.read()
+    #25
     src = cv2.imread('../Video_jalan/video_1_/video_1_ 001.jpg')
+    src = cv2.imread('../Video_jalan/video_1_/Testjpg.jpg')
     blur = cv2.GaussianBlur(src,(9,9),0)
     
     scale_percent = 50  
@@ -71,25 +74,27 @@ while(1):
     frame= cv2.resize(blur,dsize)
     crop = frame[200:360,0:640]
     
-    hsv = cv2.cvtColor(crop,cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
     warna = cv2.inRange(hsv, HSV_Low, HSV_High)
     #res = cv2.bitwise_and(frame,frame, mask = mask)
-    erosion = cv2.erode(warna,kernel,iterations = 1)
+    #erosion = cv2.erode(warna,kernel,iterations = 1)
     #dilation = cv2.dilate(mask,kernel,iterations = 1)
     #mask = cv2.morphologyEx(warna, cv2.MORPH_OPEN, kernel)
     
-    contours = cv2.findContours(warna, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+    contours = cv2.findContours(warna, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0] #connected component data
+    #spatio temporal
+    CC_O = np.zeros((len(contours)),np.int8) #connected component orientation
     for i, c in enumerate(contours):
         # Calculate the area of each contour
         area = cv2.contourArea(c)
         
         # Memilih luas kontur
-        if area <1e1 or area > 5e2 :
+        if area <4e1 or area > 100e2 :
             continue
         
         # Draw each contour only for visualisation purposes
-        cv2.drawContours(crop, contours, i, (0, 0, 255), 2)        
+        cv2.drawContours(frame, contours, i, (0, 0, 255), 1)        
         
         #cnt = contours[i]
         #rect = cv2.minAreaRect(cnt)
@@ -103,12 +108,18 @@ while(1):
         #righty = int(((cols-x)*vy/vx)+y)
         #cv2.line(crop,(cols-1,righty),(0,lefty),(0,255,0),2)
         
-        a = getOrientation(c, crop)
-        print('a =',a)
+        a = getOrientation(c, frame)
+        a_derajat = 360*a/(2*pi)
+        print(a_derajat)
+
+        #spatio tempporal
+        CC_O[i] = a_derajat
+        
+        
     
     #cv2.imshow('mask',erosion)
     cv2.imshow('hasil_Warna',warna)
-    cv2.imshow('crop',crop)
+    cv2.imshow('crop',frame)
     #cv2.imshow('res',res)
     #cv2.imshow('erot',erosion)
     #cv2.imshow('dilet',dilation)
