4fbe40acc076f6379b9e6926a1915a21c5250e27
Bakso14 <sayyidul98@gmail.com>
2020-09-29 08:34:23 +0700

    Update terbaru

--- Main_load.py
+++ Main_load.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
-from math import atan2, cos, sin
+from math import atan2, cos, sin, sqrt, pi
 def nothing(x):
     pass
-    
+
+
 def getOrientation(pts, img):
     
     sz = len(pts)
@@ -37,8 +38,8 @@ while(1):
     HSV_Low = data[0,:]
     HSV_High = data[1,:]
         
-    #_,src = cap.read()
     src = cv2.imread('../Video_jalan/video_1_/video_1_ 001.jpg')
+    #src = cv2.imread('../Video_jalan/video_1_/Testjpg.jpg')
     blur = cv2.GaussianBlur(src,(9,9),0)
     
     scale_percent = 50  
@@ -48,37 +49,51 @@ while(1):
     frame= cv2.resize(blur,dsize)
     crop = frame[200:360,0:640]
     
-    hsv = cv2.cvtColor(crop,cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
     warna = cv2.inRange(hsv, HSV_Low, HSV_High)
-    erosion = cv2.erode(warna,kernel,iterations = 1)
     
-    contours = cv2.findContours(warna, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+    contours = cv2.findContours(warna, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0] #connected component data
+    #spatio temporal
+    CC_O = np.zeros((2,len(contours)),np.int16) #connected component orientation
+    v = 0
     for i, c in enumerate(contours):
         # Calculate the area of each contour
         area = cv2.contourArea(c)
         
         # Memilih luas kontur
-        if area <1e1 or area > 5e2 :
+        if area <4e1 or area > 100e2 :
             continue
         
-        # Draw each contour only for visualisation purposes
-        cv2.drawContours(crop, contours, i, (0, 0, 255), 2)        
-        
-        cnt = contours[i]
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(crop,[box],0,(255,0,0),2)
-        
-        a = getOrientation(c, crop)
-        print('a =',a)
+        a = getOrientation(c, frame)
+        a_derajat = 360*a/(2*pi)
+        #print(a_derajat)
+
+        #spatio tempporal
+        CC_O[0,v] = a_derajat
+        CC_O[1,v] = i
+        v = v+1
+    #Pengklasteran berdasarkan orientasi
+    koreksi_o = 10
+    final_clstr = []
+    final_clstr.append([CC_O[0,0]])
+    tanda = 0
+    for wow in range(v):
+        for a in range(len(final_clstr)):
+           if CC_O[0,wow] < (final_clstr[a][0] + koreksi_o) and CC_O[0,wow] > (final_clstr[a][0] - koreksi_o):
+               final_clstr[a].append(CC_O[1,wow])
+               tanda = 0
+               break
+           else:
+              tanda = 1
+        if tanda == 1 :
+            final_clstr.append([CC_O[0,wow]])
+            final_clstr[len(final_clstr)-1].append(CC_O[1,wow])
+            tanda = 0
     
-    #cv2.imshow('mask',erosion)
+    
     cv2.imshow('hasil_Warna',warna)
-    cv2.imshow('crop',crop)
-    #cv2.imshow('res',res)
-    #cv2.imshow('erot',erosion)
-    #cv2.imshow('dilet',dilation)
+    cv2.imshow('crop',frame)
     
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
@@ -86,4 +101,3 @@ while(1):
 
 
 cv2.destroyAllWindows()
-#cap.release()
